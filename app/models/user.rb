ed9fc8d237f486afb93cc14a0288fc8aee12c43b
Chashmeet <chashmeetsingh@gmail.com>
2015-07-02 20:26:19 +0530

    new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
 	has_many :posts
 	validates_presence_of :username,:password,:email
+	after_initialize :default_values
+
+  	private
+    def default_values
+      self.avatar ||= "medium/profile.jpg"
+    end
 	mount_uploader :avatar , PhotoUploader
 	has_many :posts, through: :likes 
 end
