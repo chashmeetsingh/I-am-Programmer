98418cc2bd263d8965a27194c29d75b20bf67a42
Chashmeet Singh <chashmeetsingh@gmail.com>
2015-07-06 22:49:37 -0700

    Update ftp.rb

--- config/initializers/ftp.rb
+++ config/initializers/ftp.rb
@@ -1,5 +1,6 @@
 # Just an example 
 
+# If using ftp
 CarrierWave.configure do |config|
   config.ftp_host = "ftp.example.com"
   config.ftp_port = 21
@@ -8,4 +9,17 @@ CarrierWave.configure do |config|
   config.ftp_folder = "/public_html/uploads"
   config.ftp_url = "http://example.com/uploads"
   config.ftp_passive = false # false by default
+end
+
+# If using sftp
+
+CarrierWave.configure do |config|
+  config.sftp_host = "example.com"
+  config.sftp_user = "example"
+  config.sftp_folder = "public_html/uploads"
+  config.sftp_url = "http://example.com/uploads"
+  config.sftp_options = {
+    :password => "secret",
+    :port     => 22
+  }
 end
