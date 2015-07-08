dce0798bc6728fe7fe168c5cd27fd861004da7ed
Chashmeet <chashmeetsingh@gmail.com>
2015-07-08 19:17:00 +0530

    added validations

--- config/initializers/ftp.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.ftp_host = "ltzn.ftp.infomaniak.com"
-  config.ftp_port = 21
-  config.ftp_user = "ltzn_improgrammer"
-  config.ftp_passwd = "password"
-  config.ftp_folder = "/web/iamprogrammer"
-  config.ftp_url = "http://chashmeetsingh.com"
-  config.ftp_passive = true # false by default
-end
