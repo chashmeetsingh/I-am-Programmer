b880a111cad74784f28f38f0c4a80c0fe78894f3
Chashmeet <chashmeetsingh@gmail.com>
2015-07-08 19:07:50 +0530

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
 	
-	validates_presence_of :username,:password,:email
-	after_initialize :default_values
+	VALID_USERNAME_REGEX = /^[a-zA-Z0-9]+$/
+    validates :username, presence: true, length: { maximum: 20 },
+                                    format: { with: VALID_USERNAME_REGEX },
+                                    uniqueness: { case_sensitive: false }
+	validates :password, length: { minimum: 6 },  presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, uniqueness: true,  length: { maximum: 50 },
+	                  format: { with: VALID_EMAIL_REGEX },  presence: true
 	
-  	private
-    def default_values
-      self.avatar ||= "medium/profile.jpg"
-    end
 	mount_uploader :avatar , PhotoUploader
 
 	has_many :posts, through: :likes ,:foreign_key => :post_id
