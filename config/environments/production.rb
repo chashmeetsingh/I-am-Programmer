02d2e7c2bd1e92b5b18e80ecee8539fa7915748f
Chashmeet <chashmeetsingh@gmail.com>
2015-07-09 13:08:10 +0530

    Welcome email added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => "http://improgrammer.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   # SMTP settings for mail
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
