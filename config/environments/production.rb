6dfa1293fca901a7b088433fb9aab41d82a23501
Chashmeet <chashmeetsingh@gmail.com>
2015-07-09 13:00:53 +0530

    Welcome email added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "http://improgrammer.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for mail
   config.action_mailer.smtp_settings = {
