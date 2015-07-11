debb707aa9f001345485d5f7c75cac14ba3529d9
Chashmeet <chashmeetsingh@gmail.com>
2015-07-11 13:06:06 +0530

    Authenticity Token fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   root to: "posts#index"
   resources :posts
   #API LINKS
-  -----------------------------------------------
   get '/api/like/:user_id/:post_id' => 'api#like'
   get '/api/unlike/:user_id/:post_id' => 'api#unlike'
   get '/api/check/:user_id/:post_id' => 'api#check'
-  -----------------------------------------------
   
   get '/settings' => 'settings#settings'
   get '/profile/:username' => 'profile#profile'	
